f56cfa37f8387599d36f91e90b95f6b3f2266009
Vanessa Siordia <vsiordia117@gmail.com>
2024-06-27 19:20:40 -0600

    Portafolio actualizado

---
+++ app.js
@@ -0,0 +1,29 @@
+//Selección para barra de navegación
+const burger = document.querySelector8("#burger-menu");
+const ul = document.querySelector("nav ul");
+const nav = document.querySelector("nav")
+
+burger.addEventListener("click", () => {
+    ul.classList.toggle("show");
+});
+
+//Cerrar el menu de burger
+const navLink = document.querySelectorAll(".nav-link");
+
+navLink.forEach((link) =>
+ link.addEventListener("click",() => {
+    ul.classList.remove("show")
+ })
+);
+
+//Selección de desplazar hacia arriba
+const scrollUp = document.querySelector("#scroll-up");
+
+//Funcionalidad de desplazar hacia arriba
+scrollUp.addEventListener("click", () => {
+    window.scrollTo({
+        top: 0,
+        left: 0,
+        behavior: "smooth"
+    });
+});
